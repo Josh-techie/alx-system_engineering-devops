9ec8cd86f2a93dd6732204a5e1a765710fa94d53
Josh-techie <youse2002@outlook.fr>
2023-10-26 01:31:09 +0100

    Task Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,32 +1,41 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+"""
+Script to fetch employee tasks from a given API.
+"""
+
 import requests
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: {} <employee_id>".format(sys.argv[0]))
-        sys.exit(1)
+    if len(argv) != 2 or not argv[1].isdigit():
+        print("Usage: {} <employee_id>".format(argv[0]))
+        exit(1)
 
-    url = "https://jsonplaceholder.typicode.com/"
-    employee_id = sys.argv[1]
+    employee_id = int(argv[1])
+    base_url = "https://jsonplaceholder.typicode.com/"
+    user_url = "{}users/{}".format(base_url, employee_id)
+    todos_url = "{}todos?userId={}".format(base_url, employee_id)
 
     try:
-        user = requests.get(url + "users/{}".format(employee_id)).json()
-        todos = requests.get(url + "todos", params={"userId": employee_id}).json()
+        user_response = requests.get(user_url)
+        user_response.raise_for_status()
+        user_data = user_response.json()
 
-        if "name" not in user:
-            print("Employee not found.")
-            sys.exit(1)
+        todos_response = requests.get(todos_url)
+        todos_response.raise_for_status()
+        todos_data = todos_response.json()
 
-        completed_tasks = [task["title"] for task in todos if task["completed"]]
-        
+        completed_tasks = [task for task in todos_data if task['completed']]
+
         print("Employee {} is done with tasks({}/{}):".format(
-            user["name"], len(completed_tasks), len(todos)))
+            user_data['name'],
+            len(completed_tasks),
+            len(todos_data)
+        ))
 
         for task in completed_tasks:
-            print("\t{}".format(task))
+            print("\t {}".format(task['title']))
 
-    except requests.RequestException as e:
-        print("Error making API request:", e)
-        sys.exit(1)
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
+        exit(1)
