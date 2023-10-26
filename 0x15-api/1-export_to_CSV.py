bbec16df838dc2b9bfa3297f71ceaa19c5d1a1a5
Josh-techie <youse2002@outlook.fr>
2023-10-26 01:17:14 +0100

    Task Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,18 +1,54 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to CSV format."""
-import csv
+"""Exports to-do list information for a given employee ID to a CSV file."""
 import requests
+import csv
 import sys
 
-if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for t in todos:
-            row = [user_id, username, t.get("completed"), t.get("title")]
-            writer.writerow(row)
+def export_to_csv(employee_id, tasks):
+    filename = "{}.csv".format(employee_id)
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+
+        for task in tasks:
+            writer.writerow([employee_id, task['username'],
+                             str(task['completed']),
+                             task['title']])
+
+    print("Data exported to {}".format(filename))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: {} <employee_id>".format(sys.argv[0]))
+        sys.exit(1)
+
+    url = "https://jsonplaceholder.typicode.com/"
+    employee_id = sys.argv[1]
+
+    try:
+        user = requests.get(url + "users/{}".
+                            format(employee_id)).json()
+        todos = requests.get(url + "todos",
+                             params={"userId": employee_id}).json()
+
+        if "name" not in user:
+            print("Employee not found.")
+            sys.exit(1)
+        completed_tasks = [
+            {"username": user["username"], "completed": task["completed"],
+             "title": task["title"]}
+            for task in todos if task["completed"]
+        ]
+
+        print("Employee {} is done with tasks({}/{}):".format(
+            user["name"], len(completed_tasks), len(todos)))
+
+        for task in completed_tasks:
+            print("\t{}".format(task["title"]))
+
+        export_to_csv(employee_id, completed_tasks)
+
+    except requests.RequestException as e:
+        print("Error making API request:", e)
+        sys.exit(1)
