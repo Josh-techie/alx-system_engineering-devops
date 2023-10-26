758a1c4004ddb994c8c468aa8b7e86aa406a232d
Josh-techie <youse2002@outlook.fr>
2023-10-26 01:11:19 +0100

    Task Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,23 +8,23 @@ if __name__ == "__main__":
         print("Usage: {} <employee_id>".format(sys.argv[0]))
         sys.exit(1)
 
-    employee_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    
+    employee_id = sys.argv[1]
+
     try:
         user = requests.get(url + "users/{}".format(employee_id)).json()
         todos = requests.get(url + "todos", params={"userId": employee_id}).json()
-        
+
         if "name" not in user:
             print("Employee not found.")
             sys.exit(1)
 
-        completed = [t.get("title") for t in todos if t.get("completed")]
+        completed_tasks = [task["title"] for task in todos if task["completed"]]
         
         print("Employee {} is done with tasks({}/{}):".format(
-            user["name"], len(completed), len(todos)))
+            user["name"], len(completed_tasks), len(todos)))
 
-        for task in completed:
+        for task in completed_tasks:
             print("\t{}".format(task))
 
     except requests.RequestException as e:
