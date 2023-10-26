07aba9a08167b88067220d150b3df2c3e9a0e3cf
Josh-techie <youse2002@outlook.fr>
2023-10-26 01:09:26 +0100

    Task Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,11 +4,29 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    if len(sys.argv) != 2:
+        print("Usage: {} <employee_id>".format(sys.argv[0]))
+        sys.exit(1)
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    employee_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    
+    try:
+        user = requests.get(url + "users/{}".format(employee_id)).json()
+        todos = requests.get(url + "todos", params={"userId": employee_id}).json()
+        
+        if "name" not in user:
+            print("Employee not found.")
+            sys.exit(1)
+
+        completed = [t.get("title") for t in todos if t.get("completed")]
+        
+        print("Employee {} is done with tasks({}/{}):".format(
+            user["name"], len(completed), len(todos)))
+
+        for task in completed:
+            print("\t{}".format(task))
+
+    except requests.RequestException as e:
+        print("Error making API request:", e)
+        sys.exit(1)
