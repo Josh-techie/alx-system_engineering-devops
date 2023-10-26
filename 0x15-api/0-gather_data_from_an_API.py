c66d870b74eace031e0873c8d67ad9beba76724a
Josh-techie <youse2002@outlook.fr>
2023-10-26 01:04:08 +0100

    Task Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,22 +4,11 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: {} <user_id>".format(sys.argv[0]))
-        sys.exit(1)
-
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
     completed = [t.get("title") for t in todos if t.get("completed") is True]
-
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
-
-    # Print each task with "S" instead of spaces
-    for c in completed:
-        print("S{}".format(c.replace(" ", "S")))
-
-# Example usage:
-# python3 0-gather_data_from_an_API.py 2
+    [print("\t {}".format(c)) for c in completed]
