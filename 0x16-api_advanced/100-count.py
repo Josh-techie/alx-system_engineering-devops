548651a3ae898c5a6235a1b8a69270ba4d637f8d
Josh-techie <youse2002@outlook.fr>
2023-11-09 00:22:36 +0100

    Advanced Task try 13

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,21 +1,7 @@
-#!/usr/bin/python3
-"""
-100-count
-"""
 import requests
 
 
 def count_words(subreddit, word_list, instances={}, after=None, count=0):
-    """
-    Prints counts of given words found in hot posts of a given subreddit.
-
-    Args:
-        subreddit (str): The subreddit to search.
-        word_list (list): The list of words to search for in post titles.
-        instances (dict): Key/value pairs of words/counts.
-        after (str): The parameter for the next page of the API results.
-        count (int): The parameter of results matched thus far.
-    """
     url = f"https://www.reddit.com/r/{subreddit.lower()}/hot/.json"
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
@@ -26,17 +12,15 @@ def count_words(subreddit, word_list, instances={}, after=None, count=0):
         "limit": 100
     }
     try:
-        response = requests.get(url, headers=headers,
-                                params=params, allow_redirects=False)
-        response.raise_for_status()  # Raise an HTTPError for bad responses
+        response = requests.get(url, headers=headers, params=params,
+                                allow_redirects=False)
+        response.raise_for_status()
     except requests.exceptions.HTTPError:
-        print("")
         return
 
     results = response.json()
     data = results.get("data")
     if data is None:
-        print("")
         return
 
     after = data.get("after")
@@ -44,27 +28,16 @@ def count_words(subreddit, word_list, instances={}, after=None, count=0):
     for child in data.get("children"):
         title = child.get("data").get("title").lower().split()
         for word in set(word_list):
-            if word.lower() in title:
+            if word.lower() in title and not any(char.isalpha()
+                                                 for char in word):
                 times = title.count(word.lower())
                 instances[word] = instances.get(word, 0) + times
 
     if after is None:
         if not instances:
-            print("")
             return
-        sorted_instances = sorted(instances.items(),
-                                  key=lambda kv: (-kv[1], kv[0].lower()))
-        [print("{}: {}".format(k, v)) for k, v in sorted_instances]
+        sorted_instances = sorted(instances.items(), key=lambda
+                                  kv: (-kv[1], kv[0]))
+        [print(f"{k}: {v}") for k, v in sorted_instances]
     else:
         count_words(subreddit, word_list, instances, after, count)
-
-
-if __name__ == "__main__":
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
-        print("Ex: {} programming 'python java javascript'".
-              format(sys.argv[0]))
-    else:
-        count_words(sys.argv[1], sys.argv[2:])
