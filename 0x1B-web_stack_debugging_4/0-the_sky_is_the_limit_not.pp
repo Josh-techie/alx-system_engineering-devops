a16e4c46b1648e60fcf1b98c2c39ea4be667ab0a
Josh-techie <youse2002@outlook.fr>
2023-11-17 00:48:27 +0100

    0. Sky is the limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,16 +1,13 @@
-ky_is_the_limit_not.pp
+# Increases the amount of traffic an Nginx server can handle.
 
-# This Puppet manifest optimizes Nginx settings to handle high traffic.
-
-# Increase the ULIMIT of the default file
+Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
+command => 'sed -i "s/15/4096/" /etc/default/nginx',
+path    => '/usr/local/bin/:/bin/'
 } ->
 
-# Restart Nginx
+Restart Nginx
 exec { 'nginx-restart':
-  command => 'service nginx restart',
-  path    => '/etc/init.d/',
-  onlyif  => 'test -f /etc/init.d/nginx', # Ensure the file exists before restarting
+command => 'nginx restart',
+path    => '/etc/init.d/'
 }
